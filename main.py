635aced428f9e5cd46f08f433c0d1dc143061993
Amarnath C <amarnathcharichilil@gmail.com>
2022-05-15 22:37:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import os
-from typing import Optional
-from fastapi import FastAPI
-import uvicorn
-import random
+import asyncio
+from aiohttp import web
 
-app = FastAPI()
+routes = web.RouteTableDef()
 
-@app.get("/")
-def home():
-    return {"Hello": "World from FastAPI"}
+@routes.get("/")
+async def _home(r):
+ return web.json_response({"Hello": "World from aioHTTP"}, content_type="application/json", status=200)
 
-# get random number between min(default:0) and max(default:9)
-@app.get("/random/")
-def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
-    rval = random.randint(min, max)
-    return { "value": rval }
+async def _exec(code: str):
+  process = await asyncio.create_subprocess_shell(
+        code, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+  stdout, stderr = await process.communicate()
+  return stdout.decode().strip(), stderr.decode().strip()
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
+@routes.get("/shell")
+async def _shell(r):
+ try:
+   code = r.rel_url.query["code"]
+ except:
+   code = "echo 'No Code Provided'"
+ out, err = await _exec(code)
+ resp = {"result": {"stdout": str(out), "stderr": str(err)}}
+ return web.json_response(resp, content_type="application/json", status=200)
+
+async def start_server():
+    port = int(os.environ.get("PORT"))
+    app = web.Application()
+    app.add_routes(routes)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    await web.TCPSite(runner, "0.0.0.0", port).start()
+
+
+# Server Startup
+asyncio.get_event_loop().run_until_complete(start_server())
+print("Web Server Started.")
